4cce0a6da8485b3c92d1e0d253de6eacab06e135
DanielFlores1223 <dany-232000@hotmail.com>
2024-09-02 20:01:07 -0600

    update dependencies 9/2/2024

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
+import { ExampleResponse } from 'global-interfaces';
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req: Request, res: Response) => {
+	const response: ExampleResponse = {
+		message: 'Hello MonoRepo Api',
+		name: 'GET API ENDPOINT'
+	};
+
 	res.json({
 		message: 'hello world MonoRepo :)',
+		response
 	});
 });
 
--- packages/global-interfaces/src/index.ts
+++ packages/global-interfaces/src/index.ts
@@ -1,1 +1,9 @@
+export interface ExampleResponse {
+    name: string;
+    message: string;
+}
 
+export interface ReponseApi<T> {
+    message: string;
+    response: T;
+}
